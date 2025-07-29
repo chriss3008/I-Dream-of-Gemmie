4998fef0a52d4ac8b37addae71ef5c57399d3660
Christian Botto <christiancmpbotto@gmail.com>
2025-07-29 23:19:17 +0200

    Adicionar botão

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -11,6 +11,7 @@ const HomePage = () => {
         <a>       
             <h1>Home Page</h1>
             <p>Welcome to the Home Page!</p>    
+            <p>Treinar butão</p>    
         </a>
     )
 }
